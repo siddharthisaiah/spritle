8bf8f59b16fff376329aa747adcba0b3f37eefda
Siddharth <siddharthisaiah@gmail.com>
2019-06-19 00:21:28 +0530

    logout url

--- spritle/spritle/urls.py
+++ spritle/spritle/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', include('postcard.urls')),
     path('signup/', postcard_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
