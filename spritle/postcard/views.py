e0e5bd295f3b963e0c510f9b18be51cb5bec30d0
Siddharth <siddharthisaiah@gmail.com>
2019-06-20 02:16:01 +0530

    basic comment and postcard form processing

--- spritle/postcard/views.py
+++ spritle/postcard/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
 from .forms import SignUpForm, PostcardForm, CommentForm
 from .models import Postcard, Comment, Like
@@ -21,17 +22,32 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('postcard/home')
+            return redirect('home/')
             
     form = SignUpForm()
     return render(request, 'postcard/signup.html', {'form': form})
 
 @login_required
 def home(request):
+
     title = 'Home - Postcard'
     postcards = Postcard.objects.all()
     postcard_form = PostcardForm()
     context = {'title': title, 'postcards': postcards, 'postcard_form': postcard_form}
+    
+    if request.method == 'POST':
+        pc_form = PostcardForm(request.POST)
+        if pc_form.is_valid():
+            post = pc_form.cleaned_data['post']
+            new_postcard = Postcard(user=request.user, post=post)
+            new_postcard.save()
+
+            return redirect(reverse('home'))
+        else:
+            # TODO: redirect 404
+            pass
+        
+    
     return render(request, 'postcard/home.html', context)
 
 
@@ -41,10 +57,23 @@ def home(request):
 
 
 def postcard_detail(request, pk):
+
     title = 'Postcard - Post'
     # TODO: wrap in try catch
     postcard = Postcard.objects.filter(id=pk)[0]
     comments = Comment.objects.filter(postcard=postcard)
     comment_form = CommentForm()
     context = {'title': title, 'postcard': postcard, 'comments':comments, 'comment_form': comment_form}
+
+    if request.method == 'POST':
+        c_form = CommentForm(request.POST)
+        if c_form.is_valid():
+            comment = c_form.cleaned_data['comment']
+            new_comment = Comment(postcard=postcard, user=request.user, comment=comment)
+            new_comment.save()
+
+            return redirect('postcard_detail', pk=postcard.id)
+        else:
+            # TODO: redirect 404
+            pass
     return render(request, 'postcard/detail.html', context)
