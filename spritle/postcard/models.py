4ffa3fce6b4cbeb15dc1d1f2eb37030c2f837da0
Siddharth <siddharthisaiah@gmail.com>
2019-06-20 00:07:26 +0530

    create models for Postcard, Comment and Likes

--- spritle/postcard/models.py
+++ spritle/postcard/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Postcard(models.Model):
+    post = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+
+class Comment(models.Model):
+    postcard = models.ForeignKey(Postcard, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    postcard = models.ForeignKey(Postcard, on_delete=models.CASCADE)
+
